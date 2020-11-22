0d9154589cd1fa173a542d0e73d77c4c4325f3da
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-11-22 14:36:16 +0900

    プロフィールページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_11_22_033133) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.string "profile_photo"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
