29b9b3c1f13b664ab800b856cca796a67109beb3
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-11-23 13:58:44 +0900

    commentモデルの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :photos, dependent: :destroy
-  has_many :likes, -> { order(create_at: :desc)}, dependent: :destroy
+  has_many :photos,    dependent: :destroy
+  has_many :likes,     dependent: :destroy
+  has_many :comments,  dependent: :destroy
   accepts_nested_attributes_for :photos
 
   def liked_by(user)
